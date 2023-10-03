e34c8e7ac509f40895fc9599d66f670b3f2778dd
Timozino <timsonfoli@gmail.com>
2023-10-03 18:44:45 +0100

    made some changes

--- LMS/settings.py
+++ LMS/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
