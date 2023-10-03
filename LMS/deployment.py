df5ffa7b5dd412272b811b73c166b2c1df78a005
Timozino <timsonfoli@gmail.com>
2023-10-03 20:13:27 +0100

    made some changes

--- LMS/deployment.py
+++ LMS/deployment.py
@@ -25,7 +25,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media','media','featured_img')
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- LMS/settings.py
+++ LMS/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media', 'featured_img')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
