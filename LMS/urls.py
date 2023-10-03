52c0e83526d1479a8088298c8815158f7b131cc3
Timozino <timsonfoli@gmail.com>
2023-10-03 21:29:09 +0100

    made some changes

--- LMS/urls.py
+++ LMS/urls.py
@@ -34,4 +34,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'myapp.views.custom_not_found_view'
+handler404 = 'app.views.custom_not_found_view'
