b98ea4a6ffddfba0302d5139d288613278655174
Timozino <timsonfoli@gmail.com>
2023-10-04 02:07:18 +0100

    made some changes

--- LMS/urls.py
+++ LMS/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-#from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib import admin
 from django.urls import path, include
 from user import views
